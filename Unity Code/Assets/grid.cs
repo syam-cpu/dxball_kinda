56c6143194780451b25833d17f15d70f74556679
Aryan Kharbanda <kharbanda.aryan00@gmail.com>
2021-03-04 01:33:51 +0530

    unity code1

--- Unity Code/Assets/grid.cs
+++ Unity Code/Assets/grid.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class grid : MonoBehaviour
 {
     float timer;
-    // Vector3 startPos;
     Vector3 endPos;
 
     Vector3[] positionArray = new [] { 
@@ -28,29 +27,12 @@ public class grid : MonoBehaviour
         new Vector3(0.5f, -1.5f, 0),            
         new Vector3(1.5f, -3.5f, 0)
     };
-    // Vector3[] positionArray = new Vector3[16];
-    // positionArray[0] = new Vector3(0.5f, 1.5f, 0);            
-    // positionArray[1] = new Vector3(1.5f, 3.5f, 0);            
-    // positionArray[2] = new Vector3(0.5f, 1.5f, 0);            
-    // positionArray[3] = new Vector3(-0.5f, -1.5f, 0);            
-    
-    // positionArray[4] = new Vector3(1.5f, -0.5f, 0);
-    // positionArray[5] = new Vector3(3.5f, -1.5f, 0);            
-    // positionArray[6] = new Vector3(-1.5f, 0.5f, 0);            
-    // positionArray[7] = new Vector3(-3.5f, 1.5f, 0);            
-    
-    // positionArray[8] = new Vector3(1.5f, 0.5f, 0);            
-    // positionArray[9] = new Vector3(3.5f, 1.5f, 0);            
-    // positionArray[10] = new Vector3(-1.5f, -0.5f, 0);            
-    // positionArray[11] = new Vector3(-3.5f, -1.5f, 0);            
-    // positionArray[12] = new Vector3(-0.5f, 1.5f, 0);            
-    // positionArray[13] = new Vector3(-1.5f, 3.5f, 0);            
-    // positionArray[14] = new Vector3(0.5f, -1.5f, 0);            
-    // positionArray[15] = new Vector3(1.5f, -3.5f, 0);            
+           
 
-    int i = 0;
+    // int i = 0;
 
     void Start(){
+        Debug.Log("VFT has started");
         endPos = new Vector3(0,0,0);
         transform.position = endPos;
         StartCoroutine("follow");
@@ -67,36 +49,5 @@ public class grid : MonoBehaviour
         yield return new WaitForSeconds(5);
     }
 
-    // IEnumerator follow(){
-    //     foreach(Vector3 pos in positionArray){
-    //         StartCoroutine(move(pos));
-    //         yield return new WaitForSeconds(2);
-    //     }
-    // }
-
-    // IEnumerator move(Vector3 dest){
-    //     transform.position = dest;
-    //     yield return null;
-    // }
-
-    // void RandomPosition()
-    // {
-    //     timer = Time.time;
-    //     // startPos = transform.position;
-    //     endPos = new Vector3(Random.Range(-10f, 10f), Random.Range(-5f, 5f), 0);
-    // }
-
-    // void Update()
-    // {
-    //     if (Time.time - timer > 1)
-    //     {
-    //         // RandomPosition();
-    //         timer = Time.time;
-    //         endPos = positionArray[i];
-    //         i = (i+1)%2;
-    //     }
-    //     // transform.position = Vector3.Lerp(startPos, endPos, Time.time - timer);
-    //     transform.position = endPos;
-    // }
 
 }
